8e7f4c6efd39f2d87492b5914c3507ce84c659e8
Joao Pedro Carvalho <joao.pedro.santos.carvalho@ist.utl.pt>
2017-03-18 16:09:49 +0000

    Added searchProduct() algorithm
    related to #1

--- supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierPortImpl.java
+++ supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierPortImpl.java
@@ -50,18 +50,24 @@ public class SupplierPortImpl implements SupplierPortType {
 	}
 
 	public List<ProductView> searchProducts(String descText) throws BadText_Exception {
-		// arg verification
-		// TODO
+		// TODO: arg verification
+		
 		// cenas	
 		Supplier supplier = Supplier.getInstance();
+		List<ProductView> searchResult = new ArrayList<ProductView>();
 		for (String productId : supplier.getProductsIDs()){
 			Product product = supplier.getProduct(productId);
-			String description = product.getDescription();
-			
+			String description = product.getDescription();			
+			if(description.toLowerCase().contains(descText.toLowerCase())){
+				ProductView validProduct = new ProductView();
+				validProduct.setId(productId);
+				validProduct.setPrice(product.getPrice());
+				validProduct.setQuantity(product.getQuantity());
+				validProduct.setDesc(description);
+				searchResult.add(validProduct);
+			}			
 		}
-		
-		
-		return null;
+		return searchResult;
 	}
 
 	public String buyProduct(String productId, int quantity)
