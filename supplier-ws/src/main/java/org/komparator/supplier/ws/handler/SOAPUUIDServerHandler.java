4200b686619e7782abefc9ea78351bb1f618ba1c
Joao Pedro Carvalho <joao.pedro.santos.carvalho@ist.utl.pt>
2017-05-02 05:56:25 +0100

    fix

--- supplier-ws/src/main/java/org/komparator/supplier/ws/handler/SOAPUUIDServerHandler.java
+++ supplier-ws/src/main/java/org/komparator/supplier/ws/handler/SOAPUUIDServerHandler.java
@@ -1,10 +1,11 @@
 package org.komparator.supplier.ws.handler;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import javax.xml.namespace.QName;
 import javax.xml.soap.Name;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
@@ -12,43 +13,49 @@ import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPHeader;
 import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.handler.MessageContext.Scope;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
-public class SOAPUUIDServerHandler implements SOAPHandler<SOAPMessageContext> {
-
-    public static final String UUID_PROPERTY = "uuid.property";
-
+public class SOAPUuidServerHandler implements SOAPHandler<SOAPMessageContext> {
     public static final String REQUEST_HEADER_UUID = "requestHeaderUuid";
     public static final String REQUEST_NS = "urn:client";
 
     public static final String RESPONSE_HEADER_UUID = "responseHeaderUuid";
     public static final String RESPONSE_NS = "urn:server";
 
-    public static final String CLASS_NAME = SOAPUUIDServerHandler.class.getSimpleName();
-
     private static final String HANDLER_FLAG = "sec";
-    private boolean verbose = false;
 
+    public static final String CLASS_NAME = SOAPUuidServerHandler.class.getSimpleName();
+
+    private List<String> uuidList = new ArrayList<String>();
+
+    /**
+     * Gets the names of the header blocks that can be processed by this Handler
+     * instance. If null, processes all.
+     */
+    @Override
+    public Set getHeaders() {
+	return null;
+    }
+
+    /**
+     * The handleMessage method is invoked for normal processing of inbound and
+     * outbound messages.
+     */
     @Override
     public boolean handleMessage(SOAPMessageContext smc) {
 	Boolean outbound = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 	if (outbound) {
 	    // outbound message
 	    try {
-		SOAPEnvelope se = smc.getMessage().getSOAPPart().getEnvelope();
-		SOAPHeader sh = se.getHeader();
-		if (sh == null)
-		    sh = se.addHeader();
-		Name name = se.createName(RESPONSE_HEADER_UUID, HANDLER_FLAG, RESPONSE_NS);
-		SOAPHeaderElement element = sh.addHeaderElement(name);
-		String value = UUID.randomUUID().toString();
-		element.addTextNode(value);
-		if (verbose) {
-		    System.out.printf("%s put '%s' on response message header%n", CLASS_NAME, value);
-		}
-
+		SOAPEnvelope soapEnvelope = smc.getMessage().getSOAPPart().getEnvelope();
+		SOAPHeader soapHeader = soapEnvelope.getHeader();
+		if (soapHeader == null)
+		    soapHeader = soapEnvelope.addHeader();
+		Name uuidName = soapEnvelope.createName(RESPONSE_HEADER_UUID, HANDLER_FLAG, RESPONSE_NS);
+		SOAPHeaderElement sHeaderElement = soapHeader.addHeaderElement(uuidName);
+		String uuid = UUID.randomUUID().toString();
+		sHeaderElement.addTextNode(uuid);
 	    } catch (SOAPException e) {
 		System.out.printf("Failed to add SOAP header because of %s%n", e);
 	    }
@@ -56,33 +63,25 @@ public class SOAPUUIDServerHandler implements SOAPHandler<SOAPMessageContext> {
 	} else {
 	    // inbound message
 	    try {
-		SOAPEnvelope se = smc.getMessage().getSOAPPart().getEnvelope();
-		SOAPHeader sh = se.getHeader();
-		// check header
-		if (sh == null) {
-		    System.out.println("Header not found.");
+		SOAPEnvelope soapEnvelope = smc.getMessage().getSOAPPart().getEnvelope();
+		SOAPHeader soapHeader = soapEnvelope.getHeader();
+		if (soapHeader == null) {
+		    System.err.println("MESSAGE HAS NO HEADER");
 		    return true;
 		}
-		// get first header element
-		Name name = se.createName(REQUEST_HEADER_UUID, HANDLER_FLAG, REQUEST_NS);
-		Iterator it = sh.getChildElements(name);
-		// check header element
-		if (!it.hasNext()) {
-		    if (verbose) {
-			System.out.printf("Header element %s not found.%n", REQUEST_HEADER_UUID);
-		    }
+		Name uuidName = soapEnvelope.createName(REQUEST_HEADER_UUID, HANDLER_FLAG, REQUEST_NS);
+		Iterator elementIterator = soapHeader.getChildElements(uuidName);
+		if (!elementIterator.hasNext()) {
+		    System.err.println("MESSAGE HAS NO UUID");
 		    return true;
 		}
-		SOAPElement element = (SOAPElement) it.next();
-		String headerValue = element.getValue();
-		if (verbose) {
-		    System.out.printf("%s got '%s'%n", CLASS_NAME, headerValue);
+		SOAPElement sHeaderElement = (SOAPElement) elementIterator.next();
+		String headerValue = sHeaderElement.getValue();
+		if (uuidList.contains(headerValue)) {
+		    System.err.println("UUID ALREADY CAME");
+		    return true;
 		}
-		if (verbose) {
-		    System.out.printf("%s put '%s' on request context%n", CLASS_NAME, headerValue);
-		}
-		smc.put(UUID_PROPERTY, headerValue);
-		smc.setScope(UUID_PROPERTY, Scope.APPLICATION);
+		uuidList.add(headerValue);
 
 	    } catch (SOAPException e) {
 		System.out.printf("Failed to get SOAP header because of %s%n", e);
@@ -93,18 +92,19 @@ public class SOAPUUIDServerHandler implements SOAPHandler<SOAPMessageContext> {
 	return true;
     }
 
+    /** The handleFault method is invoked for fault message processing. */
     @Override
     public boolean handleFault(SOAPMessageContext smc) {
 	return true;
     }
 
-    @Override
-    public Set<QName> getHeaders() {
-	return null;
-    }
-
+    /**
+     * Called at the conclusion of a message exchange pattern just prior to the
+     * JAX-WS runtime dispatching a message, fault or exception.
+     */
     @Override
     public void close(MessageContext messageContext) {
+
     }
 
 }
